da64ed2b996e365729a9ecb8d4e08ce84ad7bb71
jpbelley <belleyjeanphilippe@gmail.com>
2018-05-29 23:15:47 -0400

    Gulp source maps

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ let sass = require('gulp-sass');
 let uglify = require('gulp-uglify');
 let concat = require('gulp-concat');
 let autoprefixer = require('gulp-autoprefixer');
-let cleanCSS = require('gulp-clean-css');
 let browserSync = require('browser-sync').create();
 let imagemin = require('gulp-imagemin');
 let deploy = require('gulp-gh-pages');
+let plumber      = require('gulp-plumber');
+let sourcemaps = require('gulp-sourcemaps');
 // À compléter
 // let babel = require("gulp-babel");
 
@@ -18,10 +19,18 @@ let CSS_PATH = 'src/scss/**/*.scss';
 // Translate SASS to CSS
 gulp.task('sass', function(){
   return gulp.src(CSS_PATH)
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(plumber(function(err) {
+      console.log('Style task error');
+      console.log(err);
+      this.emit('end');
+    })) // Keep gulp watch running even if the an error
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer())
     .pipe(concat('style.css'))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/css/'))
     .pipe(browserSync.reload({
       stream: true
