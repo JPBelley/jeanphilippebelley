6ccf9149af4299d4a7520c8db574ed31d4c3ecd9
jpbelley <belleyjeanphilippe@gmail.com>
2018-06-06 13:49:12 -0400

    Contact html structure

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,10 @@ gulp.task('sass', function(){
 gulp.task('scripts', function(){
   return gulp.src('src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('app/js'));
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 /* Image minification
--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,7 @@ $( document ).ready(function() {
   // General test for now, to try the nav animations
   $('#skills').on('click', function() {
     if($('body').hasClass('right')) {
-      $('body').addClass('left').removeClass('right');
-      $('.contactSection').removeClass('visible');
-      $('#skillsSection').removeClass('visible');
+      return false;
     } else {
       $('#skillsSection').addClass('visible');
       $('body').addClass('right').removeClass('left');
@@ -20,6 +18,19 @@ $( document ).ready(function() {
       $('.contactSection').removeClass('visible');
     } else {
       $('body').addClass('right').removeClass('left');
+    }
+    return false;
+  })
+
+
+  // About Section
+  $('#about').on('click', function() {
+    if($('body').hasClass('left')) {
+      return false;
+    } else {
+      console.log('there');
+      $('#skillsSection').removeClass('visible');
+      $('body').addClass('left').removeClass('right');
     }
     return false;
   })
