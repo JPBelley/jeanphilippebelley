0ba079cbeacec80bb49577480eb36fe82305fc29
jpbelley <belleyjeanphilippe@gmail.com>
2018-06-11 23:30:28 -0400

    Menu for mobile basic design

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,6 @@
 $( document ).ready(function() {
 
   // General test for now, to try the nav animations
-  $('#skills').on('click', function() {
-    if($('body').hasClass('right')) {
-      return false;
-    } else {
-      $('#skillsSection').addClass('visible');
-      $('body').addClass('right').removeClass('left');
-    }
-    return false;
-  })
-
-  // General test for now, to try the nav animations
   $('#porfolio').on('click', function() {
     if($('body').hasClass('right')) {
       $('body').addClass('left').removeClass('right');
@@ -22,6 +11,16 @@ $( document ).ready(function() {
     return false;
   })
 
+  // General test for now, to try the nav animations
+  $('#skills').on('click', function() {
+    if($('body').hasClass('right')) {
+      return false;
+    } else {
+      $('#skillsSection').addClass('visible');
+      $('body').addClass('right').removeClass('left');
+    }
+    return false;
+  })
 
   // About Section
   $('#about').on('click', function() {
@@ -29,7 +28,7 @@ $( document ).ready(function() {
       return false;
     } else {
       console.log('there');
-      $('#skillsSection').removeClass('visible');
+      $('section').removeClass('visible');
       $('body').addClass('left').removeClass('right');
     }
     return false;
