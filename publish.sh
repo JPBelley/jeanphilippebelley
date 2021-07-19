f079a8f8190a3323d949481043e895c60b05504f
Jean-Philippe Fradet-Belley <jp.fradet-belley@gsoft.com>
2021-07-18 20:19:50 -0400

    Command file publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages-photography
+rm -rf dist/
+parcel build index.html --public-url http://jeanphilippebelley.com/
+echo "jeanphilippebelley.com" > dist/CNAME
+git add dist && git commit -m "Prod files"
+git subtree push --prefix dist origin gh-pages-photo
